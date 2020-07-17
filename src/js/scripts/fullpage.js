32bdd7684d71cb8045bb961da4e4d006acb66d01
@MatevosianArutiun <arutiunmatevosian@gmail.com>
2020-07-17 04:14:19 +0300

    add_section-video

--- src/js/scripts/fullpage.js
+++ src/js/scripts/fullpage.js
@@ -1,9 +1,11 @@
 export const fullPage = () => {
   var myFullpage = new fullpage("#fullpage", {
     navigation: true,
+    slidesNavigation: true,
     scrollBar: true,
-    scrollingSpeed: 0,
-
+    scrollingSpeed: 100,
+    // scrollHorizontally: true,
+    // controlArrows: false,
     // loopBottom: true,
   });
 };
